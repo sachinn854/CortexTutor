22aa295ed1a8216f7ba5d2bb95195c10d515f859
Sachin <sachin@jettyai.cloud>
2026-04-02 01:13:58 +0530

    Remove accidental CortexTutor-hf folder

--- CortexTutor-hf/app/services/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Services module for data loading and processing."""
-
-from .youtube_loader import load_youtube_transcript, extract_video_id
-
-__all__ = ["load_youtube_transcript", "extract_video_id"]
